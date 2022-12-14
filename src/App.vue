83fd23baba386a398574070d7c448a1daf312504
mvn-tinhle-dn <tinh.le@monstar-lab.com>
2022-12-14 16:57:02 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,55 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="app">
+    <div class="linked">
+      <a href="#/">Home</a> |
+      <a href="#/todo">Todo</a> |
+      <a href="#/non-existent-path">Broken Link</a>
     </div>
-  </header>
-
-  <RouterView />
+    <component :is="currentView" />
+  </div>
 </template>
 
+<script >
+import Todo from './components/Todo.vue'
+import NotFound from './pages/NotFound.vue'
+import Home from './pages/Home.vue'
+
+
+const routes = {
+  '/': Home,
+  '/todo': Todo,
+}
+
+export default {
+  name: "App",
+  components: { Todo, NotFound, Home },
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+      console.log("sssss")
+    })
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
+#app {
+  width: 100vw;
+  height: 100vh;
   display: block;
-  margin: 0 auto 2rem;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.linked {
+  margin-bottom: 20px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <h1 class="header-title">This is first page vue js of </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style>
+
+</style>
